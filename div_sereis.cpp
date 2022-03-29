ea6328e87dba9cc1661d511f0766e54d6f9e2b31
nikita7667 <nchikhalthane@gmail.com>
2022-03-29 11:16:48 +0530

    first commit

---
+++ div_sereis.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i,n,sum;
+	cout<<"\n";
+	cout<<"enter the term:";
+	cin>>n;
+	for ( i=1; i<=n; i++)
+	{
+		sum+=i/i;
+		cout<<"i"<<"/"<<"i"<<"="<<i/i;
+		cout<<"\n";
+	}
+	cout<<"the sum of above series:"<<sum;
+	return 0;
+}
--- pattern.cpp
+++ pattern.cpp
@@ -3,16 +3,23 @@ using namespace std;
 
 int main()
 {
-	int i,j,row;
+	int row,m,i,j,k;
 	cout<<"enter the number of pattern";
 	cin>>row;
-	for ( i = row; i >= 1 ; i--)
+	m=row;
+	for ( i = 1; i <= row ; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 1; j <= m-1; j++)
 		{
-			cout<<j;
+			cout<<" ";
 		}
+		for (k=1 ; k <= 1*i-1 ; k++)
+		{
+			cout<<" *";
+		}
+		m--;
 		cout<<"\n";
 	}
 	return 0;
 }
+
